7886ca03628109f7ac6b7bb2bc8f6ac9c099ff9a
Peter Wilson <519727+peterwilsoncc@users.noreply.github.com>
2021-02-09 15:06:49 +1100

    Initial commit.

---
+++ plugin.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: WPB Scheduled Post Catcher
+Description: Catches scheduled posts that have been missed and publishes them.
+Version: 1.0
+Requires at least: 5.0
+Author: WP Beginner
+Author URI: https://wpbeginner.com
+*/
+namespace WPB\scheduledPostCatcher;
+
+require_once __DIR__ . '/inc/namespace.php';
+
+bootstrap();
